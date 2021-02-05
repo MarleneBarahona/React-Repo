ef2285d41f2d5c63b6a1072af0fe367912b3ea46
MarleneBarahona <00118616@uca.edu.sv>
2021-02-05 01:18:20 -0600

    Añadiendo primer componente de prueba

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PrimeraApp = () => {
+
+    return <h1>Hola Mundo</h1>;
+    //return <table class="table"><thead><tr><th>Proveedor</th><th>Empresa</th></tr></thead><tbody><tr><td>Proveedor X</td><td>Transportes SA de CV</td></tr></tbody></table>;
+}
+
+export default PrimeraApp;
