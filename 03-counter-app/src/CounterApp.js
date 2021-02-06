2c679549508eed70a01d53560553bbc1abf26884
MarleneBarahona <00118616@uca.edu.sv>
2021-02-05 19:12:15 -0600

    Funcionabilidad de prueba al Boton Login a estructura de prueba de pagina principal.

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import logo from './logo.png';
 
 const CounterApp = ( {value} ) => {
     return (
-        <>
+        /*<>
             <h1>CounterApp</h1>
             <h2> { value } </h2>
+            <button onClick={   function() {console.log('CLICK!')}  }>Login</button>
+        </>*/
+        <>
+            <h1>B I EN V E N I D O</h1>  
+            <div>
+                <img src={logo} className="App-logo" alt="logo" />
+            </div>
+            <button onClick={   function() {console.log('CLICK!')}  }>Login</button>
         </>
     )
 }
