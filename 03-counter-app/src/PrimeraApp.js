6ab01f5c25c3fd8ff1e74452499cb1a9b66b5ade
MarleneBarahona <00118616@uca.edu.sv>
2021-02-05 18:59:33 -0600

    Avances en Seccion 04: componentes y propTypes usando el CounterApp creado en seccion 03

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+import logo from './logo.png';
 
-const PrimeraApp = () => {
+//todos los componentes reciben PropTypes pero generalmente los reciben desestructurados, no como props en si
+const PrimeraApp = ( {saludo, subtitulo} ) => {
+    //const PrimeraApp = ( {saludo='Hola buenas'} ) => { valor por defecto
+    //const PrimeraApp = ( props ) => {
+    
+   /*const arre = [1,2,3,4,5,6];
+    const persona = {
+        nombre: 'Marlene',
+        edad: 22
+    }*/
 
-    return <h1>Hola Mundo</h1>;
-    //return <table class="table"><thead><tr><th>Proveedor</th><th>Empresa</th></tr></thead><tbody><tr><td>Proveedor X</td><td>Transportes SA de CV</td></tr></tbody></table>;
+    return (
+        <>  
+            <h1> { saludo } </h1>
+            <p> { subtitulo } </p>
+        </>
+       /* <>
+            <h1>B I EN V E N I D O</h1>  
+            <img src={logo} className="App-logo" alt="logo" />
+            <div>
+                <button type="button" class="btn">LOG IN</button>
+            </div>
+            <p>Estos son elementos de un arreglo: { arre } </p>
+            <p>Esto es un objeto: { JSON.stringify(persona) }</p>
+            <pre>Esto es un objeto con formato: { JSON.stringify( persona , null , 3) }</pre>
+        </>*/
+    ) 
+    
 }
 
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un sub'
+}
 export default PrimeraApp;
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp'
 
 import './index.css';
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render( <PrimeraApp/>, divRoot );
+//ReactDOM.render( <PrimeraApp saludo="Hola, soy Hiro!"/>, divRoot );
+ReactDOM.render( <CounterApp value={123} />, divRoot );
 
 //console.log(divRoot);
